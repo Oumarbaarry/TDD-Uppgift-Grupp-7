93f8f0c82c7178043da0279b9342feb47f0e2e58
pmodouf <pmodouf@gmail.com>
2023-09-21 13:28:01 +0200

    First Car Test

--- app/src/test/java/tdd/uppgift/grupp/CarTest.java
+++ app/src/test/java/tdd/uppgift/grupp/CarTest.java
@@ -1,7 +1,15 @@
 package tdd.uppgift.grupp;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CarTest {
+
+    @Test
+    public void testCarInstanceNotNull() {
+        Car car = new Car();
+        assertNotNull(car);
+    }
 
 }
