6a9fd8f4976734c385c90fa6bc308da9a5d896f8
schon <schon.jesper@gmail.com>
2023-09-24 09:45:26 +0200

    Commit first slide, add lights to car

--- app/src/main/java/tdd/uppgift/grupp/Car.java
+++ app/src/main/java/tdd/uppgift/grupp/Car.java
@@ -1,95 +1,15 @@
 package tdd.uppgift.grupp;
 
-public class Car implements CarInterface{
-   public boolean lightsOn;
-   public boolean backLightsOn;
-   public boolean halfLight;
-   public boolean carOn;
-   public boolean hazardLightsOn;
-   public boolean hazardlightsOf;
+public class Car  {
+  private boolean hasLights;
 
-   private boolean gasPedalPressed = false;
-   private boolean brakePedalPressed = false;
-   public int speed;
-   public boolean gasOn;
+  public Car() {
+    this.hasLights = true;
+  }
 
-
-   @Override
-   public void switchLightsOn() {
-      if(carOn){
-      this.lightsOn = true;
-   }
-   }
-   @Override
-   public void switchBackLightsOn() {
-      this.backLightsOn = true;
-   }
-   @Override
-   public void wholeLight() {
-        this.halfLight = false;
-
-
-   }
-   @Override
-   public void startCar() {
-
-        this.carOn = true;
-
-            switchLightsOn();
-
-   }
-
-   @Override
-   public void turnOffLights() {
-      this.lightsOn = false;
-      this.backLightsOn = false;
-      this.halfLight = false;
-   }
-
-    @Override
-   public void turnOffCar() {
-      this.carOn = false;
-      turnOffLights();
-   }
-
-   @Override
-   public void turnOnHazardLight() {
-      this.hazardLightsOn  = true;
-   }
-   @Override
-   public void turnOffHazardLight() {
-      this.hazardlightsOf = true;
-   }
-
-   @Override
-   public int getSpeed() {
-
-      return speed;
-   }
-   @Override
-   public void accelerate(int amount) {
-      if (carOn && gasPedalPressed) {
-         int newSpeed = speed + amount;
-         speed = Math.min(newSpeed, 180);
-      }
-   }
-
-
-
-   @Override
-   public void pressGasPedal() {
-      if (carOn) {
-         gasPedalPressed = true;
-
-      }
-   }
-   @Override
-   public void decelerate(int amount) {
-      if (carOn && brakePedalPressed) {
-         int newSpeed = speed - amount;
-         speed = Math.max(newSpeed, 0);
-      }
-   }
+  public boolean hasLights() {
+    return hasLights;
+  }
 
 }
 
--- app/src/test/java/tdd/uppgift/grupp/CarTest.java
+++ app/src/test/java/tdd/uppgift/grupp/CarTest.java
@@ -13,112 +13,12 @@ class CarTest {
     }
 
     @Test
-    public void testCarLights() {
+    public void testCarHasLights() {
         Car car = new Car();
-        assertFalse(car.lightsOn);
-
-    }
-
-    @Test
-    public void lightsOn() {
-        Car car = new Car();
-        car.switchLightsOn();
-        assertTrue(car.lightsOn);
-
-    }
-
-    @Test
-    public void backLights() {
-        Car car = new Car();
-        car.switchBackLightsOn();
-        assertTrue(car.backLightsOn);
-    }
-
-    @Test
-    public void halfLightWholeLight() {
-        Car car = new Car();
-        car.wholeLight();
-        assertFalse(car.halfLight);
-
-    }
-
-    @Test
-    public void startCar() {
-        Car car = new Car();
-        assertFalse(car.carOn);
-        car.startCar();
-        assertTrue(car.carOn);
-    }
-
-    @Test
-    public void carOffLightsOff() {
-        Car car = new Car();
-        car.turnOffCar();
-        assertFalse(car.lightsOn);
-
-
-    }
-
-    @Test
-    public void testTurnOffCar() {
-        Car car = new Car();
-        car.startCar();
-        assertTrue(car.carOn);
-        car.turnOffCar();
-        assertFalse(car.carOn);
-        assertFalse(car.lightsOn);
-        assertFalse(car.backLightsOn);
-        assertFalse(car.halfLight);
-    }
-
-    @Test
-    public void testHazardLightsOn(){
-        Car car = new Car();
-        assertFalse(car.hazardLightsOn);
-        car.turnOnHazardLight();
-        assertTrue(car.hazardLightsOn);
-
-    }
-    @Test
-    public void testTurnHazardLightsof(){
-        Car car = new Car();
-        assertFalse(car.hazardLightsOn);
-        car.turnOnHazardLight();
-        assertTrue(car.hazardLightsOn);
-        car.turnOffHazardLight();
-        assertTrue(car.hazardlightsOf);
-    }
-    @Test
-    public void testAccelerate(){
-        Car car = new Car();
-        car.startCar();
-        assertEquals(0, car.getSpeed());
-        car.accelerate(100);
-        assertEquals(100, car.getSpeed());
-
+        assertTrue(car.hasLights());
     }
 
 
-        @Test
-        public void testAccelerateToMaxSpeed() {
-            Car car = new Car();
-            car.startCar();
-            car.pressGasPedal();
-            while (car.getSpeed() < 180) {
-                car.accelerate(10);
-            }
-            assertEquals(180, car.getSpeed());
-        }
-    @Test
-    public void testDecelerateToZero() {
-        Car car = new Car();
-        car.startCar();
-        car.pressGasPedal();
-        while (car.getSpeed() > 0) {
-            car.decelerate(10);
-        }
-        assertEquals(0, car.getSpeed());
-    }
 
 }
 
