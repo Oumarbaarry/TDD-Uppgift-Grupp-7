81472635a385a6c39d5d0361422aa3d065e77251
schon <schon.jesper@gmail.com>
2023-09-24 09:52:32 +0200

    Lagt till hel & halvljus, utfört testerna.

--- app/src/main/java/tdd/uppgift/grupp/Car.java
+++ app/src/main/java/tdd/uppgift/grupp/Car.java
@@ -2,13 +2,46 @@ package tdd.uppgift.grupp;
 
 public class Car  {
   private boolean hasLights;
+  private boolean hasHighBeam;
+  private boolean hasLowBeam;
 
   public Car() {
-    this.hasLights = true;
+    this.hasLights = false;
+    this.hasHighBeam = false;
+    this.hasLowBeam = false;
   }
 
   public boolean hasLights() {
     return hasLights;
+  }
+
+  public void turnOnLights() {
+    this.hasLights = true;
+    this.hasLowBeam = true;
+  }
+
+  public void turnOffLights() {
+    this.hasLights = false;
+  }
+
+  public void setHighBeam() {
+    this.hasHighBeam = true;
+    this.hasLights = true;
+    this.hasLowBeam = false;
+  }
+
+  public void setLowBeam() {
+    this.hasLowBeam = true;
+    this.hasLights = true;
+    this.hasHighBeam = false;
+  }
+
+  public boolean hasHighBeam() {
+    return hasHighBeam;
+  }
+
+  public boolean hasLowBeam() {
+    return hasLowBeam;
   }
 
 }
--- app/src/test/java/tdd/uppgift/grupp/CarTest.java
+++ app/src/test/java/tdd/uppgift/grupp/CarTest.java
@@ -15,7 +15,37 @@ class CarTest {
     @Test
     public void testCarHasLights() {
         Car car = new Car();
+        car.turnOnLights();
         assertTrue(car.hasLights());
+    }
+
+    @Test
+    public void testTurnOnLights() {
+        Car car = new Car();
+        car.turnOnLights();
+        assertTrue(car.hasLights());
+    }
+
+    @Test
+    public void testTurnOffLights() {
+        Car car = new Car();
+        car.turnOnLights();
+        car.turnOffLights();
+        assertFalse(car.hasLights());
+    }
+
+    @Test
+    public void testSetHighBeam() {
+        Car car = new Car();
+        car.setHighBeam();
+        assertTrue(car.hasHighBeam());
+    }
+
+    @Test
+    public void testSetLowBeam() {
+        Car car = new Car();
+        car.setLowBeam();
+        assertTrue(car.hasLowBeam());
     }
 
 
